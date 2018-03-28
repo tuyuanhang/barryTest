847cb6a039116a9bf5b0f0f2d8c8371445ad98cd
Barry Tu <barry.tu@yict.com.cn>
2018-03-28 10:00:06 +0800

    add by 20180328

--- DataStructures/src/charpter5/LinkStackApp.java
+++ DataStructures/src/charpter5/LinkStackApp.java
@@ -1,5 +1,31 @@
 package charpter5;
 
+class LinkS {
+	public long dData;
+	public LinkS next;
+	
+	public LinkS(long dd) {
+		dData = dd;
+	}
+	
+	public void displayLink() {
+		System.out.print(dData + " ");
+	}
+}
+
+class LinkListS {
+	
+	private LinkS first;
+	
+	public LinkListS(){
+		first = null;
+	}
+	
+	public boolean isEmpty() {
+		return (first == null);
+	}
+}
+
 public class LinkStackApp {
 	
 	public static void main(String[] args) {
