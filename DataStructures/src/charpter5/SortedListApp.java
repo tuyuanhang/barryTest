a453863f608f555d9c822b086fce397375019fbf
Barry Tu <barry.tu@yict.com.cn>
2018-03-29 17:01:23 +0800

    0329

---
+++ DataStructures/src/charpter5/SortedListApp.java
@@ -0,0 +1,70 @@
+package charpter5;
+
+import javax.crypto.NullCipher;
+import javax.lang.model.type.NullType;
+
+class SortedList {
+	private LinkS first;
+	
+	public SortedList() {
+		first = null;
+	}
+	
+	public boolean isEmpty() {
+		return (first == null);
+	}
+	
+	public void insert(long key) {
+		LinkS newLink = new LinkS(key);
+		LinkS previous = null;
+		LinkS current = first;
+		
+		while(current != null && key > current.dData) {
+			previous = current;
+			current = current.next;
+		}
+		
+		if (previous == null ) {
+			first = newLink;
+		} else {
+			previous.next = newLink;
+		}
+		newLink.next = current;
+	}
+	
+	public LinkS remove() {
+		LinkS temp = first;
+		first = first.next;
+		return temp;
+	}
+	
+	public void displayList() {
+		System.out.print("List(first-->last):");
+		LinkS current = first;
+		while (current != null) {
+			current.displayLink();
+			current = current.next;
+		}
+		System.out.println("");
+	}
+}
+
+public class SortedListApp {
+	public static void main(String[] args) {
+		SortedList theSortedList = new SortedList();
+		theSortedList.insert(20);
+		theSortedList.insert(40);
+		
+		theSortedList.displayList();
+		
+		theSortedList.insert(10);
+		theSortedList.insert(30);
+		theSortedList.insert(50);
+		
+		theSortedList.displayList();
+		
+		theSortedList.remove();
+		
+		theSortedList.displayList();
+	}
+}
