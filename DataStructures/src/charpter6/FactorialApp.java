0bcd0f9476c9d63b2cd9c281df7d5d5c92a79ab5
Barry Tu <barry.tu@yict.com.cn>
2018-04-02 15:40:29 +0800

    0402

---
+++ DataStructures/src/charpter6/FactorialApp.java
@@ -0,0 +1,38 @@
+package charpter6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FactorialApp {
+	
+	public static int theNumber;
+	
+	public static void main(String[] args) throws IOException {
+		System.out.print("Enter a number: ");
+		theNumber = getInt();
+		int theAnswer =  factorial(theNumber);
+		System.out.println("Factorial=" + theAnswer);
+	}
+	
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return (n * factorial(n-1));
+		}
+	}
+	
+	public static String getString() throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	}
+	
+	public static int getInt() throws IOException {
+		String s = getString();
+		return Integer.parseInt(s);
+	}
+
+}
