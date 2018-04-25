3abf3de426eb41f3a7adfd8b61b945bba624dad3
Barry Tu <barry.tu@yict.com.cn>
2018-04-25 11:23:03 +0800

    0425

---
+++ DataStructures/src/charpter7/PartitionApp.java
@@ -0,0 +1,15 @@
+package charpter7;
+
+class ArrayPar {
+	private long[] theArray;
+	private int nElems;
+	
+	public ArrayPar(int max) {
+		theArray = new long[max];
+		nElems = 0;
+	}
+}
+
+public class PartitionApp {
+
+}
