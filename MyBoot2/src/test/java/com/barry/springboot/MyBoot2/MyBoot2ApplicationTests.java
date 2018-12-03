5dd748e067759be8281dfffcd90bcbcb14bb4071
Barry Tu <barry.tu@yict.com.cn>
2018-12-03 16:50:49 +0800

    add by barry

---
+++ MyBoot2/src/test/java/com/barry/springboot/MyBoot2/MyBoot2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.barry.springboot.MyBoot2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBoot2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
